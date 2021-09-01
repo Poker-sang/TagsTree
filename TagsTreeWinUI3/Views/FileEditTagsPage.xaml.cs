2a29316a8dd4f033db183130195e21b35ef65e99
Poker-sang <poker_sang@outlook.com>
2021-09-01 20:46:14 +0800

    优化为文件加标签、文件属性

---
+++ TagsTreeWinUI3/Views/FileEditTagsPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
+using TagsTreeWinUI3.Services;
+using TagsTreeWinUI3.ViewModels;
+
+namespace TagsTreeWinUI3.Views
+{
+	/// <summary>
+	/// FileEditTagsPage.xaml 的交互逻辑
+	/// </summary>
+	public partial class FileEditTagsPage : Page
+	{
+		public FileEditTagsPage()
+		{
+			_vm = new FileEditTagsViewModel();
+			InitializeComponent();
+		}
+
+		private readonly FileEditTagsViewModel _vm;
+
+		#region 事件
+
+		protected override void OnNavigatedTo(NavigationEventArgs e) => _vm.Load((FileViewModel)e.Parameter);
+		private void BackBClick(object sender, RoutedEventArgs e) => App.RootFrame.GoBack(new SlideNavigationTransitionInfo());
+
+
+		private async void AddTag(object sender, DoubleTappedRoutedEventArgs e)
+		{
+			var newTag = (TagViewModel)((TreeViewItem)sender).Tag;
+			foreach (var tagExisted in _vm.VirtualTags)
+				if (tagExisted.Name == newTag.Name)
+				{
+					MessageDialogX.Information(true, "已拥有该标签");
+					return;
+				}
+				else if (newTag.HasChildTag(tagExisted))
+				{
+					MessageDialogX.Information(true, $"已拥有下级标签「{tagExisted.Name}」");
+					return;
+				}
+				else if (tagExisted.HasChildTag(newTag))
+					if (await MessageDialogX.Warning($"将会覆盖上级标签「{tagExisted.Name}」，是否继续？"))
+					{
+						_vm.VirtualTags.Remove(tagExisted);
+						break;
+					}
+					else return;
+			_vm.VirtualTags.Add(newTag);
+			BSave.IsEnabled = true;
+		}
+
+		private void DeleteTag(object sender, RoutedEventArgs e)
+		{
+			_vm.VirtualTags.Remove((TagViewModel)((ListViewItem)sender).Tag);
+			BSave.IsEnabled = true;
+		}
+
+		private void SaveBClick(object sender, RoutedEventArgs e)
+		{
+			foreach (var tag in App.Tags.TagsDictionaryValues)
+				App.Relations[_vm.FileViewModel.GetFileModel(), tag] = _vm.VirtualTags.Contains(tag);
+			_vm.FileViewModel.TagsUpdated();
+			App.SaveRelations();
+			BSave.IsEnabled = false;
+			MessageDialogX.Information(false, "已保存更改");
+		}
+		#endregion
+	}
+}
---
+++ TagsTreeWinUI3/Views/IndexPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using TagsTreeWinUI3.Models;
+using TagsTreeWinUI3.Services;
+using TagsTreeWinUI3.Services.ExtensionMethods;
+using TagsTreeWinUI3.ViewModels;
+
+namespace TagsTreeWinUI3.Views
+{
+	public partial class IndexPage : Page
+	{
+		public IndexPage()
+		{
+			_vm = new IndexViewModel();
+			_current = this;
+			InitializeComponent();
+			Storyboard0.Begin();
+			//_ = Dispatcher.BeginInvoke(DispatcherPriority.Background, (Action)(() => Keyboard.Focus(TbSearch)));
+		}
+
+		private readonly IndexViewModel _vm;
+
+		private bool _isSearched;
+		private void Search(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs e)
+		{
+			if (_isSearched) return;
+			_isSearched = true;
+			Storyboard1.Begin();
+		}
+		private void Storyboard1_OnCompleted(object sender, object e)
+		{
+			Grid.SetRow(TbSearch, 1);
+			TbSearch.VerticalAlignment = VerticalAlignment.Top;
+			Storyboard2.Begin();
+		}
+
+		private void Storyboard2_OnCompleted(object sender, object e)
+		{
+			TbFuzzySearch.IsHitTestVisible = true;
+			DgResult.IsHitTestVisible = true;
+		}
+
+
+		#region 事件处理
+
+		private void ResultChanged(IEnumerable<FileModel> newResult) => _vm.ResultCallBack = newResult.Select(fileModel => new FileViewModel(fileModel)).ToObservableCollection();
+
+		private void TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs e) => sender.Text = Regex.Replace(sender.Text, $@"[{FileX.GetInvalidNameChars} ]+", "");
+
+		private void QuerySubmitted(AutoSuggestBox autoSuggestBox, AutoSuggestBoxQuerySubmittedEventArgs e) => _vm.FileViewModels = autoSuggestBox.Text is "" ? _vm.ResultCallBack : RelationsDataTable.FuzzySearchName(autoSuggestBox.Text, _vm.ResultCallBack);
+
+		private void OpenCmClick(object sender, RoutedEventArgs e) => ((FileViewModel)((MenuFlyoutItem)sender).Tag).FullName.Open();
+		private void OpenExplorerCmClick(object sender, RoutedEventArgs e) => ((FileViewModel)((MenuFlyoutItem)sender).Tag).Path.Open();
+		private async void RemoveCmClick(object sender, RoutedEventArgs e)
+		{
+			if (!await MessageDialogX.Warning("是否从软件移除该文件？")) return;
+			if (!App.TryRemoveFileModel((FileViewModel)((FrameworkElement)sender).Tag)) return;
+			_ = _vm.FileViewModels.Remove((FileViewModel)((FrameworkElement)sender).Tag);
+		}
+		private void PropertiesCmClick(object sender, RoutedEventArgs e) => PropertiesCmDoubleClick(((MenuFlyoutItem)sender).Tag, e);
+
+		private void PropertiesCmDoubleClick(object sender, RoutedEventArgs e) => App.RootFrame.Navigate(typeof(FilePropertiesPage), (FileViewModel)((FrameworkElement)sender).Tag);
+
+		#endregion
+
+		#region 操作
+
+		private static IndexPage _current = null!;
+		public static void FileRemoved(FileViewModel removedItem) => _ = _current._vm.FileViewModels.Remove(removedItem);
+
+		#endregion
+	}
+}
