a800b14d171b82ee8014622cc8de237cfbb13e50
Poker-sang <poker_sang@outlook.com>
2022-01-21 10:37:39 +0800

    优化项目结构

--- TagsTree/Models/FileChangedMerger.cs
+++ TagsTree/Models/FileChangedMerger.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using TagsTree.Models;
 
-namespace TagsTree.Services;
+namespace TagsTree.Models;
 
 public class FileChangedMerger
 {
--- TagsTree/Services/ExtensionMethods/Misc.cs
+++ TagsTree/Services/ExtensionMethods/Misc.cs
@@ -5,4 +5,5 @@ namespace TagsTree.Services.ExtensionMethods;
 public static class Misc
 {
     public static T CastThrow<T>(this object? obj) where T : notnull => (T)(obj ?? throw new InvalidCastException());
+    public static bool Invoke(Microsoft.UI.Dispatching.DispatcherQueueHandler dispatcherQueueHandler) => App.Window.DispatcherQueue.TryEnqueue(dispatcherQueueHandler);
 }
