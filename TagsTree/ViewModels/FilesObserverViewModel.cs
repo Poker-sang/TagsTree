e1bcebe6042013455eb596909d018b9b21fc0f5c
Poker <poker_sang@outlook.com>
2022-02-20 19:31:38 +0800

    FilesObserverPage范围删除优化

--- TagsTree/ViewModels/FilesObserverViewModel.cs
+++ TagsTree/ViewModels/FilesObserverViewModel.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections.ObjectModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
 using TagsTree.Models;
 
 namespace TagsTree.ViewModels;
 
-public class FilesObserverViewModel
+public class FilesObserverViewModel : ObservableObject
 {
     public ObservableCollection<FileChanged> FilesChangedList { get; }
+
+    public int FirstId => FilesChangedList.Count is 0 ? 0 : FilesChangedList[0].Id;
+    public int LastId => FilesChangedList.Count is 0 ? 1 : FilesChangedList[^1].Id;
 
     public FilesObserverViewModel(ObservableCollection<FileChanged> filesChangedList)
     {
@@ -18,6 +22,8 @@ public class FilesObserverViewModel
                 if (FilesChangedList.LastOrDefault() is { } fileChanged)
                     FileChanged.Num = fileChanged.Id + 1;
                 else FileChanged.Num = 1;
+            OnPropertyChanged(nameof(FirstId));
+            OnPropertyChanged(nameof(LastId));
         };
     }
 }
